d0ff415d9b7878c8a0c35ff06eea181bac586784
Marijnone <marijn.moviat@hva.nl>
2018-11-01 16:08:24 +0100

    i have the description

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const api = require('./oba-api.js')
 const chalk = require('chalk');
 const express = require('express')
+const jp = require('jsonpath')
 const app = express()
 const port = 3000
 
@@ -22,18 +23,25 @@ obaApi.get('search', {
     librarian: true,
     refine: true
   }, 'description').then(response => (
+    data = response.data
+  ))
+  .then(res => (
+    // Haal de waardes uit het object res en map daar overheen (de waarde is een array)
+    Object.values(res).map(x => {
+        //x returned een array. daar moet overheen gemapped worden
+        x.map(y => {
+          let newObject = {}
+          // Object.values haalt de eerste waarde uit het item, [0] skipt door de array blokken en ._ is de titel die je nodig hebt
+          newObject.item = Object.values(y)[0][0]._
 
-    Object.values(response).map(x => {
-      let newObject = {}
-      newObject.item = x
-      return newObject
-    })))
-  .then(res =>
+          console.log(newObject.item);
+          return newObject
+        })
 
-    filteredResponse = res
+      }
 
-  ).catch(err => console.error(err))
-
+    )).catch(err => console.error(err)))
 // Make server with the response on the port
-app.get('/', (req, res) => res.json(filteredResponse))
-app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
+app
+  .get('/', (req, res) => res.json(response))
+  .listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
