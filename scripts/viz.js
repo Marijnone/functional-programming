87c957b903379aa7dfe83f0dae97a0a08bd3d5f9
Marijnone <marijn.moviat@hva.nl>
2018-11-08 13:12:29 +0100

    sample bookshelf is working, now working on the right scale

--- scripts/viz.js
+++ scripts/viz.js
@@ -1,39 +1,45 @@
-d3.json('/json/numberOfpages.json').then(function (dataset) {
+const svg = d3.select("#chart-area")
+    .append("svg")
+    .attr("width", "1200")
+    .attr("height", "400");
 
-    console.log(dataset.description);
+d3.json('/json/numberOfpages.json').then(function (data) {
+
+    data.forEach(function (d) {
+
+    });
+    const x = d3.scaleLinear()
+        .domain([0, 800]) // the min and max of the bookpage
+        .range([0, 40]) //the range of the svg in this case the book on the shelf
+    // .paddingInner(0.3) //this is for the scaleband
+    // .paddingOuter(0.2);
+    console.log(x(300))
 
 
-    var svgWidth = 500,
-        svgHeight = 300,
-        barPadding = 5;
-    var barWidth = (svgWidth / dataset.length);
 
-    var svg = d3.select('svg')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
-
-    var barChart = svg.selectAll("rect")
-        .data(dataset)
-    console.log(dataset.description)
-
-        .enter()
-        .append("rect")
-        .attr("y", function (d) {
-            return svgHeight - d
+    const rects = svg.selectAll("rect")
+        .data(data)
+        .enter().append("rect")
+        .attr("y", 0)
+        .attr("x", function (d, i) {
+            return (i * 60);
+        })
+        //return the width of the books with d.description = nummber of pages
+        .attr("width", function (d, i) {
+            return x(d.description)
         })
         .attr("height", function (d) {
-            return d;
+            return 60;
         })
-        .attr("width", dataset.description - barPadding)
-        .attr("transform", function (d, i) {
-            var translate = [barWidth * i, 0];
-            return "translate(" + translate + ")";
+        .attr("fill", function (d) {
+            return "grey";
         });
 
+})
 
 
 
 
-});
+
 
 // <rect x="0" y="" width="20" height="50" fill="red">
